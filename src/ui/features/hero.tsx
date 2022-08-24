cb39f90a8b84912bd7cd893fec3aa153e11cb052
yoyobojo <angellolazar@gmail.com>
2022-08-24 18:02:53 -0400

    added logo and favicons

--- src/ui/features/hero.tsx
+++ src/ui/features/hero.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { Background } from '../layouts/background';
 import { Section } from '../layouts/section';
 
@@ -5,8 +6,25 @@ const Hero = () => (
   <Background color="bg-zinc-900" fullscreen>
     <Section
       yPadding="pt-20 pb-32"
+      className="min-h-inherit grid grid-cols-1 md:grid-cols-2 items-center"
     >
-      <div>Volatile Labs</div>
+      <div className="flex items-center justify-center">
+        <Image 
+          src="/assets/images/logo-bw.png"
+          alt="Volatile Labs Logo"
+          height="300"
+          width="300"
+        />
+      </div>
+      <div>
+        <h1 className="text-4xl font-bold">Volatile Labs</h1>
+        <hr className="pb-1 rounded" />
+        <div className="mt-3">
+          <p className="text-xl">
+            Some copy here...
+          </p>
+        </div>
+      </div>
     </Section>
   </Background>
 );
